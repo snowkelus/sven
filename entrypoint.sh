b6de3c7f86c8c83e554509249b6a75f9a527819f
Digital Kore <redkrakensec@wearehackerone.com>
2023-10-09 09:23:02 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-# check values
 if [ -z "${GITHUB_TOKEN}" ]; then
     echo "error: not found GITHUB_TOKEN"
     exit 1
@@ -10,19 +9,16 @@ if [ -z "${BRANCH_NAME}" ]; then
    export BRANCH_NAME=master
 fi
 
-# initialize git
 remote_repo="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 git config http.sslVerify false
 git config user.name "Automated Publisher"
 git config user.email "actions@users.noreply.github.com"
 git remote add publisher "${remote_repo}"
-git show-ref # useful for debugging
+git show-ref
 git branch --verbose
 
-# install lfs hooks
 git lfs install
 
-# publish any new files
 git checkout ${BRANCH_NAME}
 git add -A
 timestamp=$(date -u)
